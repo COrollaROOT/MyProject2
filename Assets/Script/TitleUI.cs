5c98d5d378b80931aa96271b6ff651cfc33df4f5
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-05-28 11:18:45 +0900

    [feat] Title UI
    타이틀 UI 구현
    
    한글 폰트 적용
    
    타이틀 UI 스크립트 작성하여
    버튼 상호작용 구현
    
    타이틀 이름 적용

---
+++ Assets/Script/TitleUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickStart() // 버튼을 클릭 하면 새게임
+    {
+        Debug.Log("게임 시작");
+    }
+
+    public void OnClickOption() // 버튼을 클릭 하면 옵션
+    {
+        Debug.Log("옵션");
+    }
+
+    public void OnClickQuit() // 버튼을 클릭 하면 종료
+    {
+#if UNITY_EDITOR // 에디터 상에서 게임 구동 테스트를 종료
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
+
