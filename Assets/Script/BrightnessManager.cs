2cdeead3dbca09ae4d60429094200a090b8c1357
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-05-29 17:36:50 +0900

    [feat] Brightness
    밝기 조절 창 구현
    
    UI 이미지 사용하여 간단하게 구현

---
+++ Assets/Script/BrightnessManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessManager : MonoBehaviour
+{
+    public Image brightnessOverlay; // 화면 덮는 이미지
+    public Slider brightnessSlider; // 밝기 조절 슬라이더 연결
+
+    private const string BRIGHTNESS_KEY = "Brightness"; // 저장
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float savedBrightness = PlayerPrefs.GetFloat(BRIGHTNESS_KEY, 1f); // 저장된값 불러오기
+
+        brightnessSlider.value = savedBrightness; // 슬라이더 값 초기화
+
+        ApplyBrightness(savedBrightness); // 초기 밝기
+
+        brightnessSlider.onValueChanged.AddListener(OnBrightnessChanged);
+    }
+
+    private void OnBrightnessChanged(float value)
+    {
+        ApplyBrightness(value);
+        PlayerPrefs.SetFloat(BRIGHTNESS_KEY, value);
+    }
+
+    private void ApplyBrightness(float savedBrightness)
+    {
+        float alpha = 1f - savedBrightness;
+        brightnessOverlay.color = new Color(0, 0, 0, alpha); // 오버레이 알파값 != 밝기
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
