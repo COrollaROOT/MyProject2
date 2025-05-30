f1886e0b1f454f775f94e70cde42b9b8a3238786
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-05-30 16:03:19 +0900

    [feat] Follow
    카메라가 플레이어 따라다니도록 구현

---
+++ Assets/Script/Follow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,8 +7,16 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +29,15 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("IsRun", moveVec != Vector3.zero);
+        anim.SetBool("IsWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
