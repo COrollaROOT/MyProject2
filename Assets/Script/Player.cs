71cb5e8cd2a778f2fa8bdc15d7f1ba864d6a3f2a
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-05-30 17:41:22 +0900

    [feat] Item
    Item 입수까지 구현
    
    무기 장착 및 사용 구현 예정

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] Weaponns;
+    public bool[] hasWeapons;
     float hAxis;
     float vAxis;
     bool wDown;
+    bool iDown;
 
     Vector3 moveVec;
 
     Animator anim;
+
+    GameObject nearObject;
 
     private void Awake()
     {
@@ -24,13 +29,27 @@ public class Player : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        iDown = Input.GetKeyDown(KeyCode.E);
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetInput();
+
+        Move();
+
+        Interration();
+    }
+
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
@@ -39,5 +58,36 @@ public class Player : MonoBehaviour
         anim.SetBool("IsWalk", wDown);
 
         transform.LookAt(transform.position + moveVec);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Weapon")
+
+            nearObject = other.gameObject;
+
+
+        Debug.Log(nearObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = null;
+    }
+
+    void Interration()
+    {
+        if (iDown && nearObject != null)
+        {
+            if (nearObject.tag == "Weapon")
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIdex = item.value;
+                hasWeapons[weaponIdex] = true;
+
+                Destroy(nearObject);
+            }
+        }
     }
 }
