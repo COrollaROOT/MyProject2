20ff7c152d0c2ab7a07abf813374a1c09ed225fe
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-06-04 16:37:51 +0900

    [fix] Timer
    타이틀 씬부터 타이머가 시작 되는 오류 수정
    
    로딩씬 과 연결 되있어 있던것 때문에 발생 한 오류
    
    씬 이름 과 같지 않으면 시간이 안흐르게 설정 하는것으로 버그 수정

--- Assets/Script/GameTimer.cs
+++ Assets/Script/GameTimer.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
     public TextMeshProUGUI timeText; // TextMeshPro UI 연결
+    public string targetScene = "GameScenes";
+
+    private float startTime;
+    private bool timerActive = false;
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name != targetScene)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        startTime = Time.time;
+        timerActive = true;
+
+    }
 
     void Update()
     {
-        float time = Time.time; // 게임 시작 후 경과 시간
-        int minutes = Mathf.FloorToInt(time / 60f);
-        int seconds = Mathf.FloorToInt(time % 60f);
-        float fraction = time % 1f;
+        if (!timerActive) return;
+        float elapsed = Time.time - startTime; // 게임 시작 후 경과 시간
+        int minutes = Mathf.FloorToInt(elapsed / 60f);
+        int seconds = Mathf.FloorToInt(elapsed % 60f);
+        float fraction = elapsed % 1f;
 
         timeText.text = string.Format("{0:00}:{1:00}.{2:0}", minutes, seconds,fraction * 10f);
     }
